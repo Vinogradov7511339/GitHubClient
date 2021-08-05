460e796b21c1e82b76c3731462903349a503d282
Alexander Vinogradov <vinogradov7511339@gmail.com>
2021-08-05 22:15:51 +0300

    added images

--- GitHub Client/CommonTemp/UIImage+tempExtension.swift
+++ GitHub Client/CommonTemp/UIImage+tempExtension.swift
@@ -5,4 +5,30 @@
 //  Created by Alexander Vinogradov on 05.08.2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+     static var issue: UIImage? {
+         return UIImage(systemName: "smallcircle.fill.circle")
+     }
+
+     static var pullRequest: UIImage? {
+         return UIImage(named: "icon_cell_git_pull_request")
+     }
+
+     static var releases: UIImage? {
+         return UIImage(systemName: "tag")
+     }
+
+     static var discussions: UIImage? {
+         return UIImage(systemName: "message")
+     }
+
+     static var watchers: UIImage? {
+         return UIImage(named: "icon_cell_theme")
+     }
+
+     static var license: UIImage? {
+         return UIImage(systemName: "scalemass")
+     }
+ }
