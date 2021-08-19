6215bd6703e23e855b1f6220e7e6436e43dba16f
Alexander Vinogradov <vinogradov7511339@gmail.com>
2021-08-19 20:23:45 +0300

    added new images

--- GitHub Client/Application/Resources/UIImage+Repository.swift
+++ GitHub Client/Application/Resources/UIImage+Repository.swift
@@ -11,4 +11,5 @@ extension UIImage {
     static var folder: UIImage? { UIImage(named: "rep_item_folder") }
     static var file: UIImage? { UIImage(named: "rep_item_file") }
     static var sources: UIImage? { UIImage(named: "rep_item_sources") }
+    static var star: UIImage? { UIImage(named: "rep_item_star") }
 }
