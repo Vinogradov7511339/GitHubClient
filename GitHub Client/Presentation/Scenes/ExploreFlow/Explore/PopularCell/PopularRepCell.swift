90a7cc9f79eab667aec3be32eb23d342cd8ccb03
Alexander Vinogradov <vinogradov7511339@gmail.com>
2021-08-26 05:42:45 +0300

    added  popular cell

--- GitHub Client/Presentation/Scenes/ExploreFlow/Explore/PopularCell/PopularRepCell.swift
+++ GitHub Client/Presentation/Scenes/ExploreFlow/Explore/PopularCell/PopularRepCell.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class PopularRepCell: BaseCollectionViewCell, NibLoadable {
 
+    @IBOutlet weak var avatarImageView: WebImageView!
     @IBOutlet weak var repNameLabel: UILabel!
+    @IBOutlet weak var ownerLoginLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var badgesStackView: UIStackView!
+
     override func populate(viewModel: Any) {
         super.populate(viewModel: viewModel)
         configure(viewModel: viewModel)
@@ -19,6 +24,36 @@ class PopularRepCell: BaseCollectionViewCell, NibLoadable {
 // MARK: - ConfigurableCell
 extension PopularRepCell: ConfigurableCell {
     func configure(viewModel: Repository) {
+        badgesStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+
         repNameLabel.text = viewModel.name
+        avatarImageView.set(url: viewModel.owner.avatarUrl)
+        ownerLoginLabel.text = viewModel.owner.login
+        descriptionLabel.text = viewModel.description ?? ""
+
+        if viewModel.starsCount > 0 {
+            addBadge(UIImage.RepListItem.stars, tint: .systemYellow, count: viewModel.starsCount)
+        }
+        if viewModel.forksCount > 0 {
+            addBadge(UIImage.RepListItem.forks, tint: .systemPurple, count: viewModel.forksCount)
+        }
+//        if viewModel.openIssuesCount > 0 {
+//            addBadge(UIImage.RepListItem.issues, tint: .systemGreen, count: viewModel.openIssuesCount)
+//        }
+        if viewModel.watchersCount > 0 {
+            addBadge(UIImage.RepListItem.watchers, tint: .systemBlue, count: viewModel.watchersCount)
+        }
+    }
+
+    func addBadge(_ image: UIImage?, tint: UIColor, count: Int) {
+        let imageView = UIImageView(image: image)
+        imageView.tintColor = tint
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        let formattedCount = count.roundedWithAbbreviations
+        label.text = "\(formattedCount)  "
+        badgesStackView.addArrangedSubview(imageView)
+        imageView.widthAnchor.constraint(equalToConstant: 16.0).isActive = true
+        badgesStackView.addArrangedSubview(label)
     }
 }
